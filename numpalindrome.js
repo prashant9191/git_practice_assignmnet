8ae7a68031058cfb10c55a5d25e65ce7f7877565
Abhishek-Baksi <abhisek.baksi@icloud.com>
2022-10-19 22:19:43 +0530

    Code have been corrected

--- numpalindrome.js
+++ numpalindrome.js
@@ -1,9 +1,11 @@
 let num = 1221;
 let text = num.toString();
 let bag = "";
-for (let i =0; i >=text.length; i--) {
+for (let i =text.length-1; i >=0; i--) {
   bag += text[i];
 }
 if (bag == num) {
   console.log("yes");
+}else {
+  console.log("No");
 }
